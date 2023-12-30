55642e4cd677fd8a7368507fe78e7c964edcaabc
chase <m11hut@gmail.com>
2023-12-30 03:16:56 -0800

    butcher: Shuttle_{I|II|III|IV} boatID validation based on bounding boxes, for the case where the player is moving around from Shuttle to Shuttle

--- butcher/Shuttle_IV.lua
+++ butcher/Shuttle_IV.lua
@@ -20,16 +20,38 @@ function event_waypoint_arrive(e)
         eq.stop();
         eq.move_to(3067, 877, -12, 65.27, true);
     end
-    if(e.gridid == 19 and e.wp == 11) then
+    if(e.gridid == 19 and e.wp == 12) then
         eq.spawn_condition("butcher",6,0);
         eq.spawn_condition("timorous",8,1);
         eq.debug("Shuttle to timorous (4) has reached its destination! Name is: " .. e.self:GetName() .. " Time is: " .. hour ..":" .. minute .. "", 1);
         eq.get_entity_list():ForeachClient(
             function(ent)
+                ent:Signal(4);
+            end,
+            function(ent)
+                local px = ent:GetX();
+                local py = ent:GetY();
+                local boat_id = ent:GetBoatID();
+                local diff_boat_check = boat_id == 846 or boat_id == 847 or boat_id == 848;
+                -- bounding box of Shuttle_IV
+                local valid_pos_check = px >= 3567 and px <= 3655 and py >= 520 and py <= 544;
+                -- Server thinks we're on Shuttle_I, Shuttle_II or Shuttle_III and are about to zone.
+                if(diff_boat_check and valid_pos_check) then
+                    return true;
+                end
+                return false;
+            end
+        );
+        eq.get_entity_list():ForeachClient(
+            function(ent)
                 ent:MovePC(96, -7665, 3620, 19, 72);
             end,
             function(ent)
-                if(ent:GetBoatID() == 849) then
+                local px = ent:GetX();
+                local py = ent:GetY();
+                -- bounding box of Shuttle_IV
+                local valid_pos_check = px >= 3567 and px <= 3655 and py >= 520 and py <= 544;
+                if(ent:GetBoatID() == 849 and valid_pos_check) then
                     return true;
                 end
                 return false;
--- butcher/player.lua
+++ butcher/player.lua
@@ -6,6 +6,22 @@ function event_enter_zone(e)
 	end
 end
 
+function event_signal(e)
+	if(e.signal == 1) then
+		e.self:SetBoatID(846);
+		e.self:SetBoatName("Shuttle_I000");
+	elseif(e.signal == 2) then
+		e.self:SetBoatID(847);
+		e.self:SetBoatName("Shuttle_II000");
+	elseif(e.signal == 3) then
+		e.self:SetBoatID(848);
+		e.self:SetBoatName("Shuttle_III000");
+	elseif(e.signal == 4) then
+		e.self:SetBoatID(849);
+		e.self:SetBoatName("Shuttle_IV000");
+	end
+end
+
 function event_board_boat(e)
 	local zone_time = eq.get_zone_time();
 	local hour = zone_time["zone_hour"];
