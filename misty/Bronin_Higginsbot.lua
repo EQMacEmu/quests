eb9a36ca0743f4e48d79b14a921e83f3303c64d5
regneq <rregenn@gmail.com>
2025-02-26 09:17:14 -0800

    fixed up slaythe and bronin factions and dialogues in misty

--- misty/Bronin_Higginsbot.lua
+++ misty/Bronin_Higginsbot.lua
@@ -4,24 +4,18 @@ function event_say(e)
 	elseif(e.message:findi("shard")) then
 		e.self:Say("Searching for that blasted emerald shard, are you? That thing has done nothing but bring me bad luck!! I would gladly give it to you if you would do a [small favor] for me.");
 	elseif(e.message:findi("small favor")) then
-		if(e.other:GetFaction(e.self) < 5) then
+		if(e.other:GetFactionValue(e.self) >= 15) then
 			e.self:Say("I have been tracked here by a bounty hunter named Slaythe. I have heard reports of his presence beyond the Great Wall. Please seek him out and bring me some body part of his as proof of his passing. Do this and the emerald shard is yours.");
 			if(eq.get_entity_list():IsMobSpawnedByNpcTypeID(33146) == false) then
 				 eq.spawn2(33146, 0, 0, 893.00, 664.00, -34.73, 0); -- Spawn Slaythe
 			end
-		elseif(e.other:GetFaction(e.self) < 6) then
+		elseif(e.other:GetFactionValue(e.self) >= 0) then
 			e.self:Say("When the blood of many Runnyeye goblins has covered your blade, then I shall find you worthy to speak of such matters.");
 		else
 			e.self:Say("You are no ally of the Rivervale Deeppockets!! Leave at once!!");
 		end
-	elseif(e.message:findi("second piece of the gem")) then
-		if(e.other:GetFaction(e.self) < 5) then
-			e.self:Say("The second emerald shard was hidden in the forest near Kelethin. I heard one of the local guilds found it and now call it the Gem of Tunare. You will have to ask around in Kelethin about it. Good luck.");
-		elseif(e.other:GetFaction(e.self) < 6) then
-			e.self:Say("When the blood of many Runnyeye goblins has covered your blade, then I shall find you worthy to speak of such matters.");
-		else
-			e.self:Say("You are no ally of the Rivervale Deeppockets!! Leave at once!!");
-		end
+	elseif(e.message:findi("second piece of.* gem")) then
+		e.self:Say("The second emerald shard was hidden in the forest near Kelethin. I heard one of the local guilds found it and now call it the Gem of Tunare. You will have to ask around in Kelethin about it. Good luck.");
 	end
 end
 
