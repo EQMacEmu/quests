926d70e9f886a268bbc3e46591a26fec1354e147
regneq <rregenn@gmail.com>
2025-04-09 08:45:16 -0700

    fixed syntax error for Slaythe.lua

--- misty/Slaythe.lua
+++ misty/Slaythe.lua
@@ -7,3 +7,4 @@ function event_say(e)
 			e.self:Say(eq.ChooseRandom("I didn't know Slime could speak common. Go back to the sewer before I lose my temper.","Is that your BREATH, or did something die in here? Now go away!","I wonder how much I could get for the tongue of a blithering fool? Leave before I decide to find out for myself.","Oh look, a talking lump of refuse.  How novel!"));
 		end
 	end
+end
