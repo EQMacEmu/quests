43176781a55477f397c7a08fb8aae436f8b67f35
regneq <rregenn@gmail.com>
2025-05-21 07:10:26 -0700

    fixed AI Roambox script for High Shaman. couple of dialogue fixes.

--- southkarana/#Sentry_Alechin.lua
+++ southkarana/#Sentry_Alechin.lua
@@ -1,6 +1,8 @@
 function event_say(e)
 	if(e.message:findi("hail")) then
 		e.self:Say("Please, heed my warning.  Begone before nightfall.  There are evil beasts that lurk this land. Begone!");
+	elseif(e.message:findi("joanna")) then
+		e.self:Say("I miss my people and my temple, but it is too late for me. During the day I serve Erollisi, but at night... <sob> Go! For your own good!");
 	end
 end
 
